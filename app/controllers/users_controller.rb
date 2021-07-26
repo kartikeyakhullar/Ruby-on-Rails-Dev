22cf935ead526900fd06679d5830ce15dd24ec2f
Kartikeya <kartikeya.khullar98@gmail.com>
2021-07-26 23:24:35 +0530

    Final commit on AlphaBlog.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,11 @@
 class UsersController < ApplicationController
 
+    before_action :set_user, only: [:show,:edit,:update, :destroy] 
+    before_action :require_user, only: [:edit, :update]
+    before_action :require_same_user, only: [:edit, :update, :destroy]
+
     def show
-        @user = User.find(params[:id]);
+        # @user = User.find(params[:id]);
         @articles = @user.articles
     end
 
@@ -15,11 +19,11 @@ class UsersController < ApplicationController
     end
 
     def edit
-        @user = User.find(params[:id]);
+        # @user = User.find(params[:id]);
     end
 
     def update
-        @user = User.find(params[:id]);
+        # @user = User.find(params[:id]);
         if @user.update(user_params)
             flash[:notice] = "Your account was successfully updated #{@user.username}"
             redirect_to @user
@@ -31,6 +35,7 @@ class UsersController < ApplicationController
     def create
         @user = User.new(user_params);
         if @user.save
+            session[:user_id] = @user.id
             flash[:notice] = "Welcome to the Alpha Blog #{@user.username}";
             redirect_to articles_path;
         else
@@ -38,10 +43,32 @@ class UsersController < ApplicationController
         end
     end
 
+    def destroy
+        @user.destroy
+        session[:user_id] = nil if @user == current_user
+        flash[:notice] = "Account and all associated articles successfully deleted"
+        redirect_to articles_path
+    end
+
+
     private
 
     def user_params
         params.require(:user).permit(:username, :email, :password);
+    end
+
+
+    private
+
+    def set_user
+        @user = User.find(params[:id]);
+    end
+
+    def require_same_user
+        if current_user != @user && !current_user.admin?
+            flash[:alert] = "You can only edit or delete your own account"
+            redirect_to @user
+        end
     end
 
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_24_093351) do
+ActiveRecord::Schema.define(version: 2021_07_26_132732) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 2021_07_24_093351) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "password_digest"
+    t.boolean "admin", default: false
   end
 
 end
